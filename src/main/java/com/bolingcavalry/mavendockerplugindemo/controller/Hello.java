9bdfbbe03d06f7ab1ef1eb370182e9147c88b564
liaohua1 <1045725140@qq.com>
2020-03-03 17:09:38 +0800

    修改返回内容

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "abcdefg. Hello jenkins, " + new Date();
+        return "abcdefghweweqwe. Hello jenkins, " + new Date();
     }
 }

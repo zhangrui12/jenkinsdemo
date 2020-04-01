9874b23503aad34b55c87855d2bc6fea3ee0e3ed
zhangrui12 <33524994+zhangrui12@users.noreply.github.com>
2020-04-01 16:21:36 +0800

    update controller

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -2,7 +2,7 @@ package com.bolingcavalry.mavendockerplugindemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RestController

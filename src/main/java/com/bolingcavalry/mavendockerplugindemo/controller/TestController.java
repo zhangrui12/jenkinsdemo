b1d3363c7f1d05dd265ee48454ba421784ca2ba4
liaohua1 <1045725140@qq.com>
2020-04-14 16:02:00 +0800

    add testController

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/TestController.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.bolingcavalry.mavendockerplugindemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author liaohua1
@@ -10,5 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello, liaohua" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    }
 }

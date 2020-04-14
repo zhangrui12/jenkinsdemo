bdb76fbde50c3534d5ef4c1107b2f3352e342b5d
liaohua1 <1045725140@qq.com>
2020-04-14 15:54:57 +0800

    add testController

---
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.bolingcavalry.mavendockerplugindemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liaohua1
+ * @date 2020/4/14 15:54
+ */
+@RestController
+public class TestController {
+
+    
+}
